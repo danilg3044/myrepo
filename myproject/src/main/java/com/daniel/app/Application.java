c565bc372d466508eb8ff0ef3502ab9b87b93272
Daniel Gorelic <danilg_117@yahoo.com>
2018-03-04 07:22:00 -0500

    Comment: Change of 5c88e27

--- myproject/src/main/java/com/daniel/app/Application.java
+++ myproject/src/main/java/com/daniel/app/Application.java
@@ -1,44 +1,11 @@
 package com.daniel.app;
 
-// Current : 62b9e92
-// Current : 215a6da
-// Current : 488e75a
-// Current : f0209f1
-// Current : a3888ed - from branchOne
-// Current : 700375d - from branchOne
-// Current : 10083a4 - from branchTwo
-
-// Current master is 27460df - vv
-// Current master is 87bf14e - vv
-// Current master is 27460df - dg
-// Current master is 8da72bc
-
-// Current master is 27460df - dg
-// Current master is fbff512 - dg
-// Current master is 04339c9
-// Current master is 3dea97a
-
-// 'develop' branch edited
-
-// Current develop is b5eb150
-// Current develop is 1ebb4ae
-// Current develop is 8a617dd
-
-// Current develop is fo209f1
+/*
+ * First commit of the new master
+ */
 
 public class Application {
-
 	public static void main(String[] args) {
-		System.out.printf("Hello %d\n", m1(args[0]));
-		m1(1);
-	}
-	
-	static void m1(int i) {
-		System.out.printf("number is %d\n", i);
 		
-	}
-	
-	static public int m1(String s) {
-		return s.length();
 	}
 }
