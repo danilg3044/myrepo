88f009145d0ac17508ba7ac9e4f7c838348dec28
Daniel Gorelic <danilg_117@yahoo.com>
2018-03-06 18:36:59 -0500

    aaaa

--- myproject/src/main/java/com/daniel/app/Application.java
+++ myproject/src/main/java/com/daniel/app/Application.java
@@ -1,18 +1,5 @@
 package com.daniel.app;
 
-/*
- * First commit of the new master
- * Change on brnch1
- * Change on master
- * Change on brnch2
- * Change on master
- * Another change on master
- * Another change on brnch2
- * Yet another change on brnch2
- */
-
-// external change
-
 public class Application {
 	public static void main(String[] args) {
 		
